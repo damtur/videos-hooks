cf7386f1854d27a85279935f4fbf302a476c472a
Damian Turczynski <damian.turczynski@hudl.com>
2021-03-14 15:09:42 +0000

    first commit

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+// Damian's key
+const KEY = "AIzaSyBuguv05w57msBjw7uwbh5fo3IWTJoInTw";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: KEY,
+      },
+    });
+
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+
+export default useVideos;
